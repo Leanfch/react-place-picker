e7abc2ac15335bb7e5cf1ce6f03bd7279957fd2a
Leandro Fernandez <leanf94@gmail.com>
2025-01-27 22:37:45 -0300

    add progress bar delete modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+    const [remainingTime, setRemainingTime] = useState(TIMER);
     useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime((prev) =>  prev - 10 )
+        }, 10);
+
+        
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("Timer started")
         const timer = setTimeout(() => {
             onConfirm();
-        }, 3000);
+        }, TIMER);
 
         return () => {
             clearTimeout(timer);
+            console.log("Timer cleared")
         };
     }, [onConfirm]);
     return (
@@ -22,6 +38,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
                     Yes
                 </button>
             </div>
+            <progress value={remainingTime} max={TIMER} />
         </div>
     );
 }
